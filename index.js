cc212e1ccbd4f9b74fee14937749fd7ee260e052
Yu-Chun1214 <yuchunlin0075@gmail.com>
2019-03-04 23:46:28 +0800

    finish part of website

---
+++ index.js
@@ -0,0 +1,22 @@
+// create header
+
+var setUp = function (){
+    // need to load the langPkg
+
+}
+
+var Header = function (langPkg) {
+    this.logo = 'DebuGamO';
+    this.attr = ["aboutUs", "news", "Game" , "contactUs"];
+    
+    this.rander = function () {
+
+    }
+}
+
+var loadLangPkg = function(){
+    $.ajax({
+        
+    })
+
+}
