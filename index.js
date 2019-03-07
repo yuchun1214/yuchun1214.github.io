2214c2a61a54ae1cfccbac4e0051cadad2ac31d5
Yu-Chun1214 <yuchunlin0075@gmail.com>
2019-03-07 16:59:05 +0800

    finish component width resizing for which device screen < 500px

--- index.js
+++ index.js
@@ -17,12 +17,21 @@ var Header = function (langPkg) {
 var midSizeInit = function(e){
     var bodySize = document.querySelector('body').getBoundingClientRect().width;
     // console.log(bodySize.width);
-    var jumbotronSize = document.getElementById('welcome').getBoundingClientRect().width;
+    var jumpbotron = document.getElementById('welcome');
+    var jumbotronSize = jumpbotron.getBoundingClientRect().width;
 
     var remainpx = (bodySize - jumbotronSize);
     var welcome = document.getElementById('welcome');
     welcome.style.left = remainpx / 2 + 'px';
-    console.log(jumbotronSize);
+    // console.log(bodySize);
+    if(bodySize > 1000){
+        welcome.style.top = '25%';
+    }else{
+        document.getElementById('mid').style.height = 500 + 'px';
+        welcome.style.top = '';
+    }
+    
+    // console.log(jumbotronSize);
 }
 
 var midImageEffect = function(e){
@@ -43,13 +52,26 @@ window.addEventListener('resize',function(e){
 var init = function (e){
     midSizeInit(e);
     midImageEffect(e);
+    // contentSizeInit(e);
 }
 
+// modalComponent object claim
 var modalComponent = function(type){
     this.component = document.getElementById(type + '-nav-link');
     this.form = document.getElementById(type+'form');
-    
 
+}
+
+var contentSizeInit = function(e){
+    var bodySize = document.querySelector('body').getBoundingClientRect().width;
+    if(bodySize < 500){
+        document.getElementById('about').style.padding = 50 + 'px';
+        document.getElementById('content-left').style.borderRight = '0px';
+
+    }else{
+        document.getElementById('about').style.padding = 100 + 'px';
+        document.getElementById('content-left').style.borderRight = '1px';
+    }
 }
 
 var modal = function(e){
@@ -57,3 +79,7 @@ var modal = function(e){
 }
 
 window.addEventListener('load',init);
+
+console.log('nav show');
+console.log(document.getElementById('navbarNavDropdown').className);
+;
