ac639f93163e867f8289c5a35a5ea5416f149995
Yu-Chun1214 <yuchunlin0075@gmail.com>
2019-03-09 14:11:28 +0800

    finish register form

--- index.js
+++ index.js
@@ -5,15 +5,6 @@ var setUp = function (){
 
 }
 
-var Header = function (langPkg) {
-    this.logo = 'DebuGamO';
-    this.attr = ["aboutUs", "news", "Game" , "contactUs"];
-    
-    this.rander = function () {
-
-    }
-}
-
 var midSizeInit = function(e){
     var bodySize = document.querySelector('body').getBoundingClientRect().width;
     // console.log(bodySize.width);
@@ -52,15 +43,10 @@ window.addEventListener('resize',function(e){
 var init = function (e){
     midSizeInit(e);
     midImageEffect(e);
+    registerFormInit();
     // contentSizeInit(e);
 }
 
-// modalComponent object claim
-var modalComponent = function(type){
-    this.component = document.getElementById(type + '-nav-link');
-    this.form = document.getElementById(type+'form');
-
-}
 
 var contentSizeInit = function(e){
     var bodySize = document.querySelector('body').getBoundingClientRect().width;
@@ -74,12 +60,100 @@ var contentSizeInit = function(e){
     }
 }
 
-var modal = function(e){
-
+var mask = function(){
+    var component = ['login','register'];
+    for(var i = 0; i < component.length; i++){
+        document.getElementById(component[i]).className = 'nav-link';
+        document.getElementById(component[i] + '-form').style.display = 'none';
+    }
 }
+
+var displayForm = function(e){
+    var id = this.id;
+    // console.log(this.className)
+    mask();
+    this.className = 'nav-link active';
+    
+    document.getElementById(id + '-form').style.display = 'block';
+}
+
+
+// add event listener;
+var component = ['login','register']
+for(var i = 0; i < component.length; i++){
+    document.getElementById(component[i]).addEventListener('click',displayForm)
+}
+
+
 
 window.addEventListener('load',init);
 
 console.log('nav show');
 console.log(document.getElementById('navbarNavDropdown').className);
 ;
+
+var generateYear = function(){
+    var year = '';
+    for(var i = 2019; i >= 1911; i--){
+        year += `<option class="year" value="${i}">${i}</option>`;
+    }
+    return year;
+}
+
+var generateMonth = function(){
+    var month = '';
+    for(var i = 1; i< 13; i++){
+        month += `<option class="month" value="${i}">${i}</option>`;
+    }
+    return month;
+}
+
+var generateDate = function(Year,Month){
+    if(Year != 0 && Month != 0){
+        // $('#DateNotice').remove();
+        var Fab_dates_amount = 0;
+        var dates_amount = 0;
+
+        if ((Year % 4 == 0 && Year % 100 != 0)||(Year % 400 == 0)) Fab_dates_amount = 29;
+        else Fab_dates_amount = 28;
+
+
+
+        if(Month == 2) dates_amount = Fab_dates_amount;
+        else if((Month <= 7 && Month % 2 == 1) || (Month >=8 && Month % 2 == 0))dates_amount = 31;
+        else dates_amount = 30;
+
+        date = '';
+        for(var day = 1;day <= dates_amount;day++)
+            date += `<option class="date" value="${day}">${day}</option>`
+        return date;
+    }
+}
+
+var registerFormInit = function(){
+    var yearHTML = generateYear();
+    var monthHTML = generateMonth();
+    var DateHTML = generateDate(2019,1);
+
+    $('#year').append(yearHTML);
+    $('#month').append(monthHTML);
+    $('#date').append(DateHTML);
+}
+
+$('#year').on('change',function(){
+    var year = this.value;
+    var monthHTML = generateMonth();
+    var DateHTML = generateDate(year,1);
+    $('.month').remove();
+    $('.date').remove();
+    $('#month').append(monthHTML);
+    $('#date').append(DateHTML);
+})
+
+$('#month').on('change',function(){
+    var year = document.getElementById('year').value
+    var month = this.value;
+    var DateHTML = generateDate(year,month);
+    $('.date').remove();
+    $('#date').append(DateHTML);
+})
